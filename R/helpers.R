df4f40b692e201b0d8751be58784b0e8d145b98f
Tony ElHabr <anthonyelhabr@gmail.com>
2023-05-04 06:11:57 -0500

    oh, more possession stuff

--- R/helpers.R
+++ R/helpers.R
@@ -2,7 +2,7 @@ library(arrow)
 library(fs)
 
 COMPETITION_ID <- 8
-SEASON_END_YEARS <- 2021:2023
+SEASON_END_YEARS <- 2020:2023
 TEST_SEASON_ID <- max(SEASON_END_YEARS)
 PROCESSED_DATA_DIR <- 'data/processed'
 FINAL_DATA_DIR <- 'data/final'
@@ -14,10 +14,7 @@ MODEL_ID_COLS <- c(
   'game_id',
   'action_id',
   'team_id',
-  'period_id',
-  'possession_id',
-  'within_possession_id',
-  'game_possession_id'
+  'period_id'
 )
 
 import_parquet <- function(name) {
