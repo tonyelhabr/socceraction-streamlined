853d36b8f184ecc63257e214d3d62ae47631a953
Tony ElHabr <anthonyelhabr@gmail.com>
2023-05-07 09:23:51 -0500

    prev and other cleanup in third script

--- R/3-convert-preds-to-vaep.R
+++ R/3-convert-preds-to-vaep.R
@@ -4,6 +4,14 @@ library(purrr)
 
 source(file.path('R', 'helpers.R'))
 
+## `lag` would be basically the same thing
+## `prev(c(0, 1, 2))` returns `c(1, 1, 2)`, while `lag(c(0, 1, 2))` returns `c(NA, 1, 2)`
+prev <- function(x) {
+  prev_x <- c(NA, x[-length(x)])
+  prev_x[1] <- x[1]
+  prev_x
+}
+
 ## https://github.com/ML-KULeuven/socceraction/blob/master/socceraction/vaep/formula.py
 ## https://github.com/ML-KULeuven/socceraction/blob/master/socceraction/atomic/vaep/formula.py
 get_vaep_prev_actions <- function(
@@ -13,22 +21,22 @@ get_vaep_prev_actions <- function(
     result_name,
     concedes,
     scores,
-    pov = 'offense',
+    pov = 'scores',
     same_phase_seconds = 10
 ) {
-  same_team <- lag(team_id) == team_id
-  if (pov == 'offense') {
+  same_team <- prev(team_id) == team_id
+  if (pov == 'scores') {
     pos <- scores
     neg <- concedes
-  } else if (pov == 'defense') {
+  } else if (pov == 'concedes') {
     pos <- concedes
     neg <- scores
   }
-  prev_actions <- lag(pos) * same_team + lag(neg) * !same_team
-  too_long_idx <- abs(time_seconds - lag(time_seconds)) > same_phase_seconds
+  prev_actions <- prev(pos) * same_team + prev(neg) * !same_team
+  too_long_idx <- abs(time_seconds - prev(time_seconds)) > same_phase_seconds
   prev_actions[too_long_idx] <- 0
   
-  prev_goal_idx <- (lag(type_name) %in% c('shot', 'shot_freekick', 'shot_penalty')) & (lag(result_name) == 'success')
+  prev_goal_idx <- (prev(type_name) %in% c('shot', 'shot_freekick', 'shot_penalty')) & (prev(result_name) == 'success')
   prev_actions[prev_goal_idx] <- 0
   prev_actions
 }
@@ -38,48 +46,48 @@ get_vaep_atomic_prev_actions <- function(
     type_name,
     concedes,
     scores,
-    pov = 'offense'
+    pov = 'scores'
 ) {
-  same_team <- lag(team_id) == team_id
-  if (pov == 'offense') {
+  same_team <- prev(team_id) == team_id
+  if (pov == 'scores') {
     pos <- scores
     neg <- concedes
-  } else if (pov == 'defense') {
+  } else if (pov == 'concedes') {
     pos <- concedes
     neg <- scores
   }
-  prev_actions <- lag(pos) * same_team + lag(neg) * !same_team
+  prev_actions <- prev(pos) * same_team + prev(neg) * !same_team
   
-  prev_goal_idx <- lag(type_name) %in% c('goal', 'owngoal')
+  prev_goal_idx <- prev(type_name) %in% c('goal', 'owngoal')
   prev_actions[prev_goal_idx] <- 0
   prev_actions
 }
 
 get_ovaep_value <- function(type_name, scores, ...) {
-  prev_actions <- get_vaep_prev_actions(pov = 'offense', type_name = type_name, scores = scores, ...)
+  prev_scores <- get_vaep_prev_actions(pov = 'scores', type_name = type_name, scores = scores, ...)
   penalty_idx <- type_name == 'shot_penalty'
-  prev_actions[penalty_idx] <- 0.792453
+  prev_scores[penalty_idx] <- 0.792453
   
   # fixed odds of scoring when corner
   corner_idx <- type_name %in% c('corner_crossed', 'corner_short')
-  prev_actions[corner_idx] <- 0.046500
+  prev_scores[corner_idx] <- 0.046500
   
-  coalesce(scores - prev_actions, 0)
+  coalesce(scores - prev_scores, 0)
 }
 
 get_dvaep_value <- function(concedes, ...) {
-  prev_actions <- get_vaep_prev_actions(pov = 'defense', concedes = concedes, ...)
-  coalesce(-(concedes - prev_actions), 0)
+  prev_concedes <- get_vaep_prev_actions(pov = 'concedes', concedes = concedes, ...)
+  coalesce(-(concedes - prev_concedes), 0)
 }
 
 get_ovaep_atomic_value <- function(scores, ...) {
-  prev_actions <- get_vaep_atomic_prev_actions(pov = 'offense', scores = scores, ...)
-  coalesce(scores - prev_actions, 0)
+  prev_scores <- get_vaep_atomic_prev_actions(pov = 'scores', scores = scores, ...)
+  coalesce(scores - prev_scores, 0)
 }
 
 get_dvaep_atomic_value <- function(concedes, ...) {
-  prev_actions <- get_vaep_atomic_prev_actions(pov = 'defense', concedes = concedes, ...)
-  coalesce(-(concedes - prev_actions), 0)
+  prev_concedes <- get_vaep_atomic_prev_actions(pov = 'concedes', concedes = concedes, ...)
+  coalesce(-(concedes - prev_concedes), 0)
 }
 
 
